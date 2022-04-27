5e77117202d08703358c096273d451c09e73f843
shreonk1 <joeytqpr@gmail.com>
2022-04-27 17:38:42 +0100

    Extra -help updates

--- app/commands/Misc/help.js
+++ app/commands/Misc/help.js
@@ -8,6 +8,24 @@ module.exports = {
     name: 'help',
     description: 'Displays all commands and options for the discord bot',
     execute(message, args) {
-        message.channel.send(">>> **Shrub's help menu**\n > Commands: \n - _**Admin Only**_ \n > Ban - Bans a user permanently from the discord. \n > Tempban - Bans a user temporarily from the discord. \n > Kick - kicks a user from the discord. \n > Mute - mutes a user permanently. \n > Tempmute - Mutes a user temporarily. \n > Unban - unbans a banned user. \n > Unmute - unmutes a muted user. \n - _**miscellaneous**_ \n > help - provides a list of all current commands for users. \n > adminsay - makes the bot say something.(**ADMIN**) \n > allsay - a say command for everyone. Mentions are disabled! \n - _**Roles**_ \n > createrole - creates a role with designated colours and name.(**ADMIN**) \n > giverole - gives a user a role. (**ADMIN**) \n > roles - prints out a list of all current roles. \n > takerole - takes a role from a user. (**ADMIN**)")
+        message.channel.send(`>>> **Shrub's help menu**  
+      **Commands** : 
+      - _**Admin Only**_ 
+         > Ban - Bans a user permanently from the discord. 
+         > Tempban - Bans a user temporarily from the discord. 
+         > Kick - kicks a user from the discord. 
+         > Mute - mutes a user permanently. 
+         > Tempmute - Mutes a user temporarily. 
+         > Unban - unbans a banned user. 
+         > Unmute - unmutes a muted user. 
+      - _**Miscellaneous**_ 
+         > help - provides a list of all current commands for users. 
+         > adminsay - makes the bot say something.(**ADMIN**) 
+         > allsay - a say command for everyone. Mentions are disabled! 
+      - _**Roles**_ 
+         > createrole - creates a role with designated colours and names.(**ADMIN**) 
+         > giverole - gives a user a role. (**ADMIN**) 
+         > roles - prints out a list of all current roles. 
+         > takerole - takes a role from a user. (**ADMIN**)`)
     }
 }
